bbd68550e8f28e84358f3cf5214767377c0f27e5
Washington Soares <washingtonudi@gmail.com>
2016-10-16 01:22:55 -0200

    Criando Main

--- projection/src/main/java/projection/technique/lsp/Main.java
+++ projection/src/main/java/projection/technique/lsp/Main.java
@@ -1,8 +1,26 @@
 package projection.technique.lsp;
 
+import matrix.MatrixFactory;
+import matrix.AbstractMatrix;
+import distance.dissimilarity.AbstractDissimilarity;
+import distance.dissimilarity.DissimilarityFactory;
+import distance.dissimilarity.DissimilarityFactory.DissimilarityType;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String a[]){
-        System.out.println("lol");
+    static {
+        System.load("/Users/Washington/projects/tcc/libs VCS/liblspsolver.so");
+    }
+    public static void main(String a[]) throws IOException {
+        LSPProjection2D lspProjection2D = new LSPProjection2D();
+        String dataSetPath = "/Users/Washington/Dropbox/IC/datasets_v3/B1Q.data";
+        DissimilarityType dissimilarityType  = DissimilarityType.EUCLIDEAN;
+
+        AbstractMatrix matrix = MatrixFactory.getInstance(dataSetPath);
+        AbstractDissimilarity diss = DissimilarityFactory.getInstance(dissimilarityType);
+
+        AbstractMatrix result = lspProjection2D.project(matrix, diss);
+        System.out.println(System.getProperty("java.library.path"));
     }
 }
