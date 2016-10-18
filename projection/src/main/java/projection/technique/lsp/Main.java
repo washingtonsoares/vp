f466f969b3a7b9e342fc90092a0dae3f333bbb34
Washington Soares <washingtonudi@gmail.com>
2016-10-18 09:41:43 -0200

    Mudanças no Main

--- projection/src/main/java/projection/technique/lsp/Main.java
+++ projection/src/main/java/projection/technique/lsp/Main.java
@@ -8,9 +8,10 @@ import distance.dissimilarity.DissimilarityFactory.DissimilarityType;
 
 import java.io.IOException;
 
-
-
 public class Main {
+    static {
+        System.load("/Users/Washington/projects/tcc/libs-vp/liblspsolver.so");
+    }
     public static void main(String a[]) throws IOException {
         LSPProjection2D lspProjection2D = new LSPProjection2D();
         String dataSetPath = "/Users/Washington/Dropbox/IC/datasets_v3/B1Q.data";
@@ -20,5 +21,6 @@ public class Main {
         AbstractDissimilarity diss = DissimilarityFactory.getInstance(dissimilarityType);
 
         AbstractMatrix result = lspProjection2D.project(matrix, diss);
+        System.out.println(System.getProperty("java.library.path"));
     }
 }
